6f294ca13aaca522abfae1ea9e796a1d357407ed
Kanickai.R <Kanickai.R@EXIND3010-146.allfon.in>
2022-03-21 15:05:08 +0530

    update 2

---
+++ jenkins-git-maven-docker-build/src/main/java/com/example/demo/controller/TeacherController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class TeacherController {
+
+	
+	
+	@GetMapping(path = "/teachers")
+	public String getTeachers() {
+		
+		List<String> list =Arrays.asList("kanic","selva");
+		return list.toString();
+	}
+	
+	@GetMapping(path = "/teachers/{dept}")
+	public String getTeachersByDept(@PathVariable("dept") String dept) {
+		
+		List<String> list =Arrays.asList("kanic","selva");
+		
+		if(dept.equals("ece")) {
+			list=Arrays.asList("raj","dharshini");
+		}
+		return list.toString();
+	}
+}
